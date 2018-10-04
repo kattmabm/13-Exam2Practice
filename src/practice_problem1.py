50e1ca3bc4bc65bc2037af6563803bbe048ce5f6
kattmabm <kattmabm@rose-hulman.edu>
2018-10-04 10:36:41 -0400

    TODO 4, 5

--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -39,11 +39,11 @@ def main():
     # UN-comment tests as you work the problems.
     ####################################################################
 
-    run_test_init()
-    run_test_append_string()
+#     run_test_init()
+#     run_test_append_string()
 #     run_test_double()
 #     run_test_shrink()
-#     run_test_double_then_shrink()
+    run_test_double_then_shrink()
 #     run_test_reset()
 #     run_test_steal()
 #     run_test_get_history()
@@ -140,7 +140,7 @@ class Box(object):
           :type additional_contents: str
         """
         # --------------------------------------------------------------
-        # TODO: 3. Implement and test this function.
+        # DONE: 3. Implement and test this function.
         #     See the testing code (below) for more examples.
         # --------------------------------------------------------------
         # --------------------------------------------------------------
@@ -207,7 +207,7 @@ class Box(object):
           #                       contents that did NOT fit]
         """
         # --------------------------------------------------------------
-        # TODO: 4. Implement and test this function.
+        # DONE: 4. Implement and test this function.
         #     The testing code is already written for you (above).
         # --------------------------------------------------------------
         # --------------------------------------------------------------
@@ -219,6 +219,8 @@ class Box(object):
         # FOR FULL CREDIT, YOUR SOLUTION MUST BE NO MORE THAN
         #    ** TWO **   LINES OF CODE.
         ################################################################
+
+        return Box.append_string(self, self.contents)
 
     def shrink(self, new_volume):
         """
@@ -256,7 +258,7 @@ class Box(object):
           :type new_volume: int
         """
         # --------------------------------------------------------------
-        # TODO: 5. Implement and test this function.
+        # DONE: 5. Implement and test this function.
         #     The testing code is already written for you (above).
         # --------------------------------------------------------------
         # --------------------------------------------------------------
@@ -267,6 +269,18 @@ class Box(object):
         # IMPORTANT: Write a solution to this problem in pseudo-code,
         # and THEN translate the pseudo-code to a solution.
         # --------------------------------------------------------------
+
+        clipped = ''
+        shrunk = ''
+        for k in range(len(self.contents)):
+            if k < new_volume:
+                shrunk += self.contents[k]
+            else:
+                clipped += self.contents[k]
+        self.contents = shrunk
+        self.volume = new_volume
+        return clipped
+
 
     def double_then_shrink(self, new_volume):
         """
@@ -320,6 +334,10 @@ class Box(object):
         #    DIFFICULTY:      5
         #    TIME ESTIMATE:   5 minutes.
         # --------------------------------------------------------------
+
+        doubled_box = Box.double(self)
+        shrunk_box = Box.shrink(doubled_box, new_volume)
+
 
     def reset(self):
         """
